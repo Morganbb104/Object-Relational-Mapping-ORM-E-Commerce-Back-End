9cf49a0d511bbd1c1d9d1dcb23862035e279eb62
CHENG CHIEN <hcc780921@gmail.com>
2022-03-21 13:49:16 -0700

    define tag dataType

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,20 +1,23 @@
-const { Model, DataTypes } = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
-
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
+class ProductTag extends Model {} ProductTag.init({ // define columns
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    tag_name: {
+        type: DataTypes.STRING
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
-  }
-);
+    modelName: 'product_tag'
+});
 
 module.exports = ProductTag;
